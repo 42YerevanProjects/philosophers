3229ac2732b7fccfd8527816c3e7e2e7df45f64e
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-08 07:38:53 -0400

    The main file

---
+++ philo_one/src/main.c
@@ -0,0 +1,11 @@
+#include "../includes/philo_one.h"
+
+int	main(int argc, char **argv)
+{
+	t_data *data;
+
+	data = (t_data *)malloc(sizeof(t_data));
+	check_args(argc, argv);
+	printf("Here\n");
+	return (0);
+}
