9ef963333ca7caca64c4f32274161201d662f287
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-10 02:03:37 -0400

    Thea main file

--- philo_one/src/main.c
+++ philo_one/src/main.c
@@ -1,11 +1,36 @@
 #include "../includes/philo_one.h"
 
+static void	init_data(t_data *data, int argc, char **argv)
+{
+	char	**args;
+	int		i;
+
+	i = 0;
+	if (argc == 2)
+		args = ft_split(argv[1], ' ');
+	else
+	{
+		i = 1;
+		args = argv;
+	}
+	data->philo_n = ft_atoi(args[i]);
+	data->die_t = ft_atoi(args[i + 1]);
+	data->eat_t = ft_atoi(args[i + 2]);
+	data->sleep_t = ft_atoi(args[i + 3]);
+	if (args[i + 4] != NULL)
+		data->eat_n = ft_atoi(args[i + 4]);
+	if (argc == 2)
+		ft_free(args);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data *data;
 
 	data = (t_data *)malloc(sizeof(t_data));
 	check_args(argc, argv);
-	printf("Here\n");
+	init_data(data, argc, argv);
+	printf("%d\n", data->philo_n);
+	free(data);
 	return (0);
 }
