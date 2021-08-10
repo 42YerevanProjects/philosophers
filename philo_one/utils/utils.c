7d374d1f1bad25f303aa59da6b7f805ca711e2a4
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-10 02:04:22 -0400

    The util functions

--- philo_one/utils/utils.c
+++ philo_one/utils/utils.c
@@ -22,6 +22,33 @@ void	ft_free(char **str)
 		free(str[i--]);
 }
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+int		ft_nbrlen(long n)
+{
+	int		size;
+
+	size = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		size++;
+	while (n != 0)
+	{
+		n = n / 10;
+		size++;
+	}
+	return (size);
+}
+
 long	ft_atoi(const char *str)
 {
 	long	i;
