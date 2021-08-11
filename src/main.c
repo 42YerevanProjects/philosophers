b4639f460279d824ec842e628a8c5500faa74b4c
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-11 15:57:24 -0400

    Inmplemented and nitialized Philosopher's struct

--- src/main.c
+++ src/main.c
@@ -1,37 +1,21 @@
 #include "../includes/philosophers.h"
 
-static void	init_data(t_data *data, int argc, char **argv)
-{
-	char	**args;
-	int		i;
-
-	i = 0;
-	if (argc == 2)
-		args = ft_split(argv[1], ' ');
-	else
-	{
-		i = 1;
-		args = argv;
-	}
-	data->philo_n = ft_atoi(args[i]);
-	data->die_t = ft_atoi(args[i + 1]);
-	data->eat_t = ft_atoi(args[i + 2]);
-	data->sleep_t = ft_atoi(args[i + 3]);
-	if (args[i + 4] != NULL)
-		data->eat_n = ft_atoi(args[i + 4]);
-	if (argc == 2)
-		ft_free(args);
-}
-
 int	main(int argc, char **argv)
 {
-	t_data *data;
+	t_philo	*philos;
+	t_mutex	*mutex;
+	t_data	*data;
 
-	data = (t_data *)malloc(sizeof(t_data));
 	if (check_args(argc, argv) != 0)
 		return (-1);
+
+	mutex = (t_mutex *)malloc(sizeof(t_mutex));
+	data = (t_data *)malloc(sizeof(t_data));
+
 	init_data(data, argc, argv);
-	printf("%d\n", data->philo_n);
-	free(data);
+	init_mutex(mutex, data);
+	philos = init_threads(data, mutex);
+
+	printf("%d\n", philos[0].index);
 	return (0);
 }
