744e0dada8fca7f781ab56780d23ce889de38846
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-11 08:06:19 -0400

    Initial commit

---
+++ utils/time.c
@@ -0,0 +1,19 @@
+#include "../includes/philosophers.h"
+
+long	get_time_ms(void)
+{
+	struct timeval	t;
+
+	gettimeofday(&t, NULL);
+	return ((t.tv_sec * 1000) + (t.tv_usec / 1000));
+}
+
+void	ft_usleep(int length)
+{
+	long	time;
+
+	time = get_time_ms();
+	while(get_time_ms() < time + length)
+		usleep(length);
+}
+
---
+++ utils/utils.c
@@ -0,0 +1,70 @@
+#include "../includes/philosophers.h"
+
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+void	ft_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	while (i >= 0)
+		free(str[i--]);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+int		ft_nbrlen(long n)
+{
+	int		size;
+
+	size = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		size++;
+	while (n != 0)
+	{
+		n = n / 10;
+		size++;
+	}
+	return (size);
+}
+
+long	ft_atoi(const char *str)
+{
+	long	i;
+	long	number;
+	int		sign;
+
+	i = 0;
+	number = 0;
+	sign = 1;
+	while (str[i] && (str[i] == 32 || (str[i] >= 9 && str[i] <= 13)))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		number = (number * 10) + (str[i] - '0');
+		i++;
+	}
+	return (number * sign);
+}
+
