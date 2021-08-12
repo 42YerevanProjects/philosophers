81d3b760d96bf27c8e63f82744adb499d3e9e7d6
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-12 13:38:45 -0400

    Implemented initializing structs and created launch threads and join threads functions

--- src/main.c
+++ src/main.c
@@ -1,5 +1,47 @@
 #include "../includes/philosophers.h"
 
+static int launch_threads(t_philo *philos)
+{
+	int	i;
+	int	size;
+	int	ret;
+
+	i = 0;
+	size = philos[0].data->philo_n;
+	while (i < size)
+	{
+		ret = pthread_create(&philos[i].t_id, NULL, create_simulation, &philos[i]);
+		if (ret != 0)
+		{
+			printf("Something went wrong while creating a thread\n");
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	join_threads(t_philo *philos)
+{
+	int	i;
+	int size;
+	int	ret;
+
+	i = 0;
+	size = philos[0].data->philo_n;
+	while (i < size)
+	{
+		ret = pthread_join(philos[i].t_id, NULL);
+		if (ret != 0)
+		{
+			printf("Something went wrong while creating a thread\n");
+			return (-1);
+		}
+		i++;	
+	}
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philo	*philos;
@@ -16,6 +58,9 @@ int	main(int argc, char **argv)
 	init_mutex(mutex, data);
 	philos = init_threads(data, mutex);
 
-	printf("%d\n", philos[0].index);
+	launch_threads(philos);
+	join_threads(philos);
+
 	return (0);
 }
+
