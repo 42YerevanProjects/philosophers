2c687190d7157d0e14ec71726fe6d8b36a8102b6
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-16 09:03:37 -0400

    Fixed the problems with forks giving deadlock

--- src/simulation.c
+++ src/simulation.c
@@ -4,8 +4,6 @@ static int	is_dead(t_philo *philo)
 {
 	if (philo->last_meal == 0)
 		return (0);
-	printf("starving: %ld\n", (get_time_ms() - philo->last_meal));
-	printf("die: %d\n", philo->data->die_t);
 	if ((get_time_ms() - philo->last_meal) < philo->data->die_t)
 		return (0);
 	return (1);
@@ -13,17 +11,17 @@ static int	is_dead(t_philo *philo)
 
 static void	philosopher_eat(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->r_fork);
+	pthread_mutex_lock(philo->r_fork);
+	pthread_mutex_lock(philo->l_fork);
 	print_status(philo, "has taken a fork");
-	pthread_mutex_lock(&philo->l_fork);
 	print_status(philo, "has taken a fork");
 	pthread_mutex_lock(&philo->mutex->m_write);
 	print_status(philo, "is eating");
-	usleep(philo->data->eat_t * 1000);
 	philo->last_meal = get_time_ms();
+	usleep(philo->data->eat_t * 1000);
 	pthread_mutex_unlock(&philo->mutex->m_write);
-	pthread_mutex_unlock(&philo->l_fork);
-	pthread_mutex_unlock(&philo->r_fork);
+	pthread_mutex_unlock(philo->l_fork);
+	pthread_mutex_unlock(philo->r_fork);
 }
 
 static void	philosopher_sleep_and_think(t_philo *philo)
