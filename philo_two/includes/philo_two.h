7d811f4856309efccab3b17bdd9411e31e39074c
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-17 16:05:38 -0400

    The header file

---
+++ philo_two/includes/philo_two.h
@@ -0,0 +1,63 @@
+#ifndef PHILO_TWO_H
+# define PHILO_TWO
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <semaphore.h>
+
+typedef	struct s_semaphore
+{
+	sem_t	*fork;
+	sem_t	*write;
+	sem_t	*end;
+}				t_semaphore;
+
+typedef struct s_data
+{
+	int	philo_n;
+	int	die_t;
+	int	eat_t;
+	int	sleep_t;
+	int	eat_count;
+}			t_data;
+
+typedef struct s_philo
+{
+	pid_t			pid;
+	int				index;
+	long long		born_time_ms;
+	long long		last_meal;
+	t_semaphore		*sem;
+	t_data			*data;
+
+}				t_philo;
+
+// Util functions
+int			ft_isdigit(int c);
+void		ft_free(char **str);
+long		ft_atoi(const char *str);
+int			ft_nbrlen(long n);
+size_t		ft_strlen(const char *s);
+char		**ft_split(const char *s, char c);
+void		ft_error(char *msg);
+void		check_args(int argc, char **argv);
+
+long long	get_time_ms(void);
+void		ft_usleep(int length);
+
+// Sources
+void		init_semaphore(t_semaphore *sem, int len);
+void		init_data(t_data *data, int argc, char **argv);
+t_philo		*init_philos(t_data *data, t_semaphore *sem);
+void		simulation_cycle(t_philo *philo);
+void    	print_status(t_philo *philo, char *msg);
+
+#endif
+
