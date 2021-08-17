3f303b2239bb7a1a4a96aa1eb51c0e2ec6ceaa6b
Nathanael <sargishovsepyan2001@gmail.com>
2021-08-17 16:07:15 -0400

    The simulation functions

---
+++ philo_one/src/simulation.c
@@ -0,0 +1,62 @@
+#include "../includes/philosophers.h"
+
+static int	is_dead(t_philo *philo)
+{
+	if (philo->last_meal == 0)
+		return (0);
+	//printf("dite time : %d\n", philo->data->die_t);
+	//printf ("Passed : %d\n", get_time_ms() - philo->last_meal);
+	if ((get_time_ms() - philo->last_meal) <= philo->data->die_t)
+		return (0);
+	return (1);
+}
+
+static void	philosopher_eat(t_philo *philo)
+{
+	pthread_mutex_lock(philo->r_fork);
+	pthread_mutex_lock(philo->l_fork);
+	philo->last_meal = get_time_ms();
+	print_status(philo, "has taken a fork");
+	print_status(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->mutex->m_write);
+	print_status(philo, "is eating");
+	pthread_mutex_unlock(&philo->mutex->m_write);
+	usleep(philo->data->eat_t * 1000);
+	pthread_mutex_unlock(philo->l_fork);
+	pthread_mutex_unlock(philo->r_fork);
+}
+
+static void	philosopher_sleep_and_think(t_philo *philo)
+{
+	print_status(philo, "is sleeping");
+	usleep(philo->data->sleep_t * 1000);
+	print_status(philo, "is thinking");
+}
+
+void	*create_simulation(void	*philosopher)
+{
+	t_philo	*philo;
+	int		limit;
+	int		*ret;
+
+	philo = (t_philo *) philosopher;
+	limit = philo->data->eat_count;
+	ret = malloc(sizeof(int));
+	*ret = 0;
+	while (limit && !philo->data->dead)
+	{
+		
+		philosopher_eat(philo);
+		philosopher_sleep_and_think(philo);
+		if (limit != -1)
+			limit--;
+		if (is_dead(philo) && limit == -1)
+		{	
+			*ret = philo->index;
+			philo->data->dead = 1;
+			return ((void *)ret);
+		}
+	}
+	return ((void *)ret);
+}
+
---
+++ philo_two/src/simulation.c
@@ -0,0 +1,59 @@
+#include "../includes/philo_two.h"
+
+static void	*check_is_alive(void *ptr)
+{
+	t_philo		*philo;
+
+	philo = (t_philo *)ptr;
+	while (1)
+	{
+		sem_wait(philo->sem->end);
+		if (get_time_ms() - philo->last_meal > philo->data->die_t)
+		{
+			print_status(philo, "died");
+			kill(0, SIGINT);
+			return (NULL);
+		}
+		sem_post(philo->sem->end);
+		usleep(1000);
+	}
+	return (NULL);
+}
+
+static void	philosopher_eat(t_philo *philo)
+{
+	sem_wait(philo->sem->fork);
+	print_status(philo, "has taken a fork");
+	print_status(philo, "has taken a fork");
+	philo->last_meal = get_time_ms();
+	print_status(philo, "is eating");
+	usleep(philo->data->eat_t * 1000);
+	sem_post(philo->sem->fork);
+}
+
+static void	philosopher_sleep_and_think(t_philo *philo)
+{
+	print_status(philo, "is sleeping");
+	usleep(philo->data->sleep_t * 1000);
+	print_status(philo, "is thinking");
+}
+
+void	simulation_cycle(t_philo *philo)
+{
+	int			loop;
+	pthread_t	thread_id;
+
+	loop = philo->data->eat_count;
+	philo->last_meal = get_time_ms();
+	pthread_create(&thread_id, NULL, &check_is_alive, (void *)philo);
+	while (loop != 0)
+	{
+		philosopher_eat(philo);
+		philosopher_sleep_and_think(philo);
+		usleep(1000);
+		if (loop != -1)
+			loop--;
+	}
+	exit(0);
+}
+
