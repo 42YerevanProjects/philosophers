e347b404157bdd3ef945e6da90f9c51f54119314
Sargis Hovsepyan <shovsepy@c1r8s3.42yerevan.am>
2021-08-19 19:00:09 +0400

    The print status function

--- philo/src/message.c
+++ philo/src/message.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   message.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 18:40:09 by shovsepy          #+#    #+#             */
+/*   Updated: 2021/08/19 18:40:11 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo.h"
 
 int	print_status(t_philo *philo, char *msg)
 {
 	int		index;
 	long	time;
-	
+
 	index = philo->index;
 	time = get_time_ms() - philo->born_time_ms;
 	printf("%ld #%d %s\n", time, index, msg);
--- philo_bonus/src/message.c
+++ philo_bonus/src/message.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   message.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shovsepy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 18:46:39 by shovsepy          #+#    #+#             */
+/*   Updated: 2021/08/19 18:46:42 by shovsepy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/philo_bonus.h"
 
 void    print_status(t_philo *philo, char *msg)
@@ -9,4 +21,3 @@ void    print_status(t_philo *philo, char *msg)
 	time = get_time_ms() - philo->born_time_ms;
 	printf("%ld #%d %s\n", time, index, msg);
 }
-
