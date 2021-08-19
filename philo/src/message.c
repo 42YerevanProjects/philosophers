1b629a655c60b8bdc220725430baaec71b8f7312
Sargis Hovsepyan <shovsepy@c1r8s3.42yerevan.am>
2021-08-19 17:26:12 +0400

    Modified the print status to only print when no one id dead

--- philo/src/message.c
+++ philo/src/message.c
@@ -1,11 +1,13 @@
 #include "../includes/philo.h"
 
-void	print_status(t_philo *philo, char *msg)
+int	print_status(t_philo *philo, char *msg)
 {
 	int		index;
 	long	time;
-
+	if (philo->data->dead && philo->data->death_count != 1)
+		return (-1);
 	index = philo->index;
 	time = get_time_ms() - philo->born_time_ms;
 	printf("%ld #%d %s\n", time, index, msg);
+	return (0);
 }
